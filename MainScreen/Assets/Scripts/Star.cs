9b77a57bd1d230d7243472979e96973c67cf4bb7
decentninja <very@decent.ninja>
2016-02-23 16:29:17 +0100

    First iteration of the force directed graph.

---
+++ MainScreen/Assets/Scripts/Star.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Star : MonoBehaviour {
+
+	public Star parent;
+	public LineRenderer line;
+	public SpringJoint spring;
+	public Rigidbody rb;
+	float savedOffMinDistance;
+	float savedOffMaxDistance;
+
+	void Start () {
+		if(parent) {
+			spring.connectedBody = parent.rb;
+			line.enabled = true;
+		} else {
+			rb.constraints |= RigidbodyConstraints.FreezePosition;
+			savedOffMinDistance = spring.minDistance;
+			savedOffMaxDistance = spring.maxDistance;
+			spring.minDistance = 0;
+			spring.maxDistance = 0;
+		}
+	}
+
+	public void SetParent(Star other) {
+		line.enabled = true;
+		rb.constraints = RigidbodyConstraints.FreezeRotation;
+		parent = other;
+		spring.connectedBody = parent.rb;
+		spring.minDistance = savedOffMinDistance;
+		spring.maxDistance = savedOffMaxDistance;
+	}
+	
+	void Update () {
+		if(parent) {
+			line.SetPosition(0, transform.position);
+			line.SetPosition(1, parent.transform.position);
+		}
+	}
+}
