8f9a81638e40d96f4b1dcc4fcb4c07a8de474729
Victor Larsson <victor@dictodesigns.com>
2016-02-26 00:35:28 +0100

    Control panel V1

--- ControlPanel/www/js/controllers.js
+++ ControlPanel/www/js/controllers.js
@@ -1,10 +1,32 @@
 angular.module('starter.controllers', [])
 
-/*
-*	Main controller
-*/
-.controller("mainCtrl", ['$scope', function($scope) {
+.controller("mainCtrl", ['$scope', 'Repositories', function($scope, Repositories) {
 
-	$scope.variable = "Test";
+	$scope.repositories = Repositories.all();
+}])
+
+/*
+*	Repository controller
+*/
+.controller("repositoryCtrl", ['$scope', '$stateParams', 'Repositories', '$timeout', function($scope, $stateParams, Repositories, $timeout) {
+
+	$scope.repository = Repositories.byId($stateParams.id);
+	$scope.totalDuration = 100; // In days
+	$scope.rewindDays = 0;
+	$scope.intensityDuration = 7;
+	$scope.lightning = false;
+
+	$scope.rewindNumberOfDays = function(days) {
+		$scope.rewindDays = days;
+		console.log($scope.rewindDays);
+	}
+
+	$scope.triggerLightning = function() {
+		console.log("THUNDER!");
+		$scope.lightning = true;
+		$timeout(function() {
+			$scope.lightning = false;
+		}, 750);
+	}
 
 }]);
