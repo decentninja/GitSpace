1e98c40d3ec4e661fc46ab9af445efad64e49598
decentninja <very@decent.ninja>
2016-03-02 16:50:39 +0100

    Large improvement to line rendering

---
+++ MainScreen/Assets/Scripts/DrawLines.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawLines : MonoBehaviour {
+
+	public Material mat;
+	public Repositories repositories;
+
+	void OnPostRender() {
+		GL.PushMatrix();
+		mat.SetPass(0);
+		foreach(Transform child in repositories.transform) {
+			foreach(Transform grandchild in child) {
+				Folder folder = grandchild.GetComponent<Folder>();
+				if(folder != null) {
+					GL.Begin(GL.LINES);
+					GL.Vertex(folder.transform.position);
+					GL.Vertex(folder.parent.transform.position);
+					GL.End();
+				}
+			}
+		}
+		GL.PopMatrix();
+	}
+
+}
--- MainScreen/Assets/Scripts/Folder.cs
+++ MainScreen/Assets/Scripts/Folder.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class Folder : MonoBehaviour {
 
 	public GameObject parent;
-	public LineRenderer line;
 	public SpringJoint spring;
 	public Rigidbody rb;
 	public int size;
@@ -13,8 +12,4 @@ public class Folder : MonoBehaviour {
 		spring.connectedBody = parent.GetComponent<Rigidbody>();
 	}
 
-	void Update () {
-		line.SetPosition(0, transform.position);
-		line.SetPosition(1, parent.transform.position);
-	}
 }
