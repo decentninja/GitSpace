77e747f13b073114effea2465e76925d7988fe3d
decentninja <very@decent.ninja>
2016-03-02 13:57:22 +0100

    Delete on new update

--- MainScreen/Assets/Scripts/Repositories.cs
+++ MainScreen/Assets/Scripts/Repositories.cs
@@ -7,23 +7,23 @@ using System.Collections.Generic;
 public class Repositories : MonoBehaviour {
 
     private Dictionary<string, Repository> repoDictonary = new Dictionary<string, Repository>();
+
     public GameObject repoPrefab;
 
 	public void handle(JsonData data) {
         string type = (string) data["type"];
+	string repoName;
         switch (type) {
             case "command":
 
                 break;
             case "delete":
-
+                repoName = (string) data["repo"];
+		dierepo(repoName);
                 break;
             case "state":
-                string repoName = (string)data["repo"];
-                if (repoDictonary.ContainsKey(repoName))
-                {
-                    repoDictonary.Remove(repoName);
-                }
+                repoName = (string) data["repo"];
+		dierepo(repoName);
                 Repository newRepo = Instantiate(repoPrefab).GetComponent<Repository>();
                 newRepo.transform.parent = transform;
                 repoDictonary.Add(repoName, newRepo);
@@ -40,6 +40,14 @@ public class Repositories : MonoBehaviour {
                 return;
         }
 
+    }
+
+    private void dierepo(string repoName) {
+	if (repoDictonary.ContainsKey(repoName))
+	{
+	    Destroy(repoDictonary[repoName]);
+	    repoDictonary.Remove(repoName);
+	}
     }
 
 }
