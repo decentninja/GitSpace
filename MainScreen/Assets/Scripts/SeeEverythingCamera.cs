5a2b93688c66001049721a11b0a22eb91df5fa99
decentninja <very@decent.ninja>
2016-03-09 14:47:58 +0100

    That missing camera lerp and a polestar mesh

--- MainScreen/Assets/Scripts/SeeEverythingCamera.cs
+++ MainScreen/Assets/Scripts/SeeEverythingCamera.cs
@@ -5,6 +5,7 @@ public class SeeEverythingCamera : MonoBehaviour {
 
 	public Transform target;
 	public float margin = 1;
+	public float cameraspeed = 1;
 	Camera camera;
 
 	void Start() {
@@ -12,11 +13,13 @@ public class SeeEverythingCamera : MonoBehaviour {
 	}
 
 	void Update () {
-		Bounds bounds = AndreasAwesomeHelperSuperLibrary.CalculateTotalBounds(target);
-		float height = bounds.size.z;
-		Vector3 pos = bounds.center;
-		pos.y = -height * 0.5f / Mathf.Tan(camera.fieldOfView * 0.5f * Mathf.Deg2Rad);
-		pos.y *= margin;
-		transform.position = pos;
+		if(target != null) {
+			Bounds bounds = AndreasAwesomeHelperSuperLibrary.CalculateTotalBounds(target);
+			float height = bounds.size.z;
+			Vector3 pos = bounds.center;
+			pos.y = -height * 0.5f / Mathf.Tan(camera.fieldOfView * 0.5f * Mathf.Deg2Rad);
+			pos.y *= margin;
+			transform.position = Vector3.Lerp(transform.position, pos, cameraspeed * Time.deltaTime);
+		}
 	}
 }
