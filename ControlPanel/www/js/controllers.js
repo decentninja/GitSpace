502bd5e03cd8d9c66851bdeb96fcb58632a6582a
Victor Larsson <victor@dictodesigns.com>
2016-03-10 21:55:53 +0100

    Updated interaction from meeting

--- ControlPanel/www/js/controllers.js
+++ ControlPanel/www/js/controllers.js
@@ -3,18 +3,66 @@ angular.module('gitSpace.controllers', [])
 .controller("mainCtrl", ['$scope', '$rootScope', 'Repositories', function($scope, $rootScope, Repositories) {
 
 	$scope.repositories = Repositories.all();
+	$scope.rewindDays = 0;
+	$scope.activityThreshold = 12;
+	$scope.visuals = {
+		labels: false
+	};
 
 	$rootScope.$on("dataAvailable", function() {
 		$scope.repositories = Repositories.all();
 		console.log("Repositories loaded!");
+		console.log($scope.repositories);
+		for(var i in $scope.repositories) {
+			$scope.repositories[i].show = false;
+		}
 		$scope.$apply();
 	});
 
 	$scope.zoomRepository = function(repository) {
-		Repositories.emit('repo focus', {
-			command: 'repo focus',
-			repo: repository.name
+		// Close all others
+		for(var i in $scope.repositories) {
+			if($scope.repositories[i] != repository) {
+				$scope.repositories[i].show = false;
+			}
+		}
+		if(!repository.show) {
+			// We are focusing on a repo!
+			Repositories.emit('repo focus', JSON.stringify({
+				command: 'repo focus',
+				repo: repository.name
+			}));
+			repository.show = true;
+		} else {
+			// We are closing a repo
+			Repositories.emit('overview', JSON.stringify({
+				command: 'overview'
+			}));
+			repository.show = false;
+		}
+	};
+
+	$scope.setActivityThreshold = function(newActivityThreshold) {
+		var minutes = newActivityThreshold * 60; // We recieve hours
+		Repositories.emit("activity threshold", JSON.stringify({
+			command: 'activity threshold',
+			threshold: minutes
+		}));
+	};
+
+	$scope.setLabels = function(bool) {
+		Repositories.emit("labels", {
+			command: 'labels',
+			show: bool
 		});
+	};
+
+	$scope.setUserActivity = function(userData) {
+		Repositories.emit('user activity', JSON.stringify({
+			command: 'user activity',
+			name: userData.name,
+			mail: userData.mail
+		}));
 	};
 
 }])
@@ -26,11 +74,6 @@ angular.module('gitSpace.controllers', [])
 
 	$scope.repository = Repositories.byName($stateParams.name);
 	$scope.initialized = ($scope.repository !== null) ? true : false;
-	$scope.rewindDays = 0;
-	$scope.activityThreshold = 12;
-	$scope.visuals = {
-		labels: false
-	};
 
 	$rootScope.$on("dataAvailable", function() {
 		console.log("Repositories loaded!");
@@ -38,28 +81,5 @@ angular.module('gitSpace.controllers', [])
 		$scope.initialized = true;
 		$scope.$apply();
 	});
-
-	$scope.setActivityThreshold = function(newActivityThreshold) {
-		var minutes = newActivityThreshold * 60; // We recieve hours
-		Repositories.emit("activity threshold", {
-			command: 'activity threshold',
-			threshold: minutes
-		});
-	};
-
-	$scope.setLabels = function(bool) {
-		Repositories.emit("labels", {
-			command: 'labels',
-			show: bool
-		});
-	};
-
-	$scope.setUserActivity = function(userData) {
-		Repositories.emit('user activity', {
-			command: 'user activity',
-			name: userData.name,
-			mail: userData.mail
-		});
-	};
 
 }]);
