a6464f088fafc1ee4b7ac5755657e70f93accf50
Mattias Lönnerberg <mattias.lonnerberg@gmail.com>
2016-03-16 16:47:58 +0100

    Prepared HUD timer for timetravel

--- MainScreen/Assets/Scripts/HUD.cs
+++ MainScreen/Assets/Scripts/HUD.cs
@@ -6,11 +6,30 @@ using System;
 public class HUD : MonoBehaviour {
 
 	public Text time;
+    public bool isRealtime = true;
 
-	public void setTime(int unixtime) { }
+	public void setTime(int unixtime) {
+        isRealtime = false;
+        DateTime datetime = UnixTimeStampToDateTime(unixtime);
+        time.text = datetime.ToString("yyyy.MM.dd HH:mm");
+    }
+
+    public void setToRealTime() {
+        isRealtime = true;
+    }
 
 	void Update() {
-		DateTime datetime = DateTime.Now;
-		time.text = datetime.ToString("yyyy.MM.dd HH:mm");
+        if (isRealtime) {
+		    DateTime datetime = DateTime.Now;
+		    time.text = datetime.ToString("yyyy.MM.dd HH:mm");
+        }
 	}
+
+    public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+    {
+        // Unix timestamp is seconds past epoch
+        DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+        dtDateTime = dtDateTime.AddSeconds(unixTimeStamp - 3600).ToLocalTime(); // -3600 to Adjust for timezone
+        return dtDateTime;
+    }
 }
