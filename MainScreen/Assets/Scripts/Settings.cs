2cd7c846e21d995ca04897b31a952843cfb8bb10
decentninja <very@decent.ninja>
2016-03-17 15:23:41 +0100

    Screen selector

---
+++ MainScreen/Assets/Scripts/Settings.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Options {
+	public string host;
+}
+
+public class Settings : MonoBehaviour {
+
+	public InputField hostfield;
+	public Options options;
+	public GameObject chooser;
+
+	void Start () {
+		DontDestroyOnLoad(transform.gameObject);
+		hostfield.Select();
+	}
+	
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Return)) {
+			Connect();
+		}
+	}
+
+	public void Connect() {
+		options = new Options();
+		if(hostfield.text.Length != 0) {
+			options.host = hostfield.text;
+		} else {
+			options.host = "127.0.0.1";
+		}
+		chooser.SetActive(false);
+		SceneManager.LoadScene("Mainscreen");
+	}
+}
